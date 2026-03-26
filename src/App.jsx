3f853bbc11427ca6e7eb8b472ba9522c0317f2f4
Tony <kounleadissa@gmail.com>
2026-03-26 02:22:24 +0100

    add display placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,18 @@ const App = () => {
     <div className='min-h-screen bg-gray-950 flex items-center justify-center px-4'>
      <div className='bg-gray-900 text-gray-200 w-full max-w-md border rounded-2xl shadow-xl border-gray-800 p-8'>
           
-        <h1 className='text-2xl font-bold text-center mb-5 '>
+        <h1 className='text-2xl font-bold text-center mb-5'>
           Password Generetor
         </h1>
+      
+      { /* Password Display */}
+      <div className='flex mb-4'>
+         <input type="text" placeholder='Generated Password' className='flex-1 bg-gray-800 border border-gray-700 
+         rounded-l-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500' />
+      </div>
+
+
+
 
        </div>
     </div>
