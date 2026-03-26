aaba0850e960817964a41f014efec512577eb58b
Tony <kounleadissa@gmail.com>
2026-03-26 02:36:12 +0100

    add a password length function

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,26 @@ const App = () => {
       
       { /* Password Display */}
       <div className='flex mb-4'>
+
          <input type="text" placeholder='Generated Password' className='flex-1 bg-gray-800 border border-gray-700 
          rounded-l-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500' readOnly />
 
-        
-           <button className='bg-indigo-600 hover:bg-indigo-700 px-4 rounded-r-lg transition'>
+          <button className='bg-indigo-600 hover:bg-indigo-700 px-4 rounded-r-lg transition'>
             Copy
            </button>
-
       </div>
+
+     { /* Password Length */}
+      <div className='mb-4'>
+ 
+         <label className='block mb-2 text-5m font-medium'> Password Length </label>
+          <input 
+          type="range" 
+          min="6" 
+          max="32"
+          className="w-full accent-indigo-600"/>
+      </div>
+
 
      </div>
     </div>
