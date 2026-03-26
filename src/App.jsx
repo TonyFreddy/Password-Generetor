4d5effa06181891a7835b6fc1e77cf70de95d098
Tony <kounleadissa@gmail.com>
2026-03-26 02:14:32 +0100

    add title

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-950 flex items-center justify-center px-4'>
+     <div className='bg-gray-900 text-gray-200 w-full max-w-md border rounded-2xl shadow-xl border-gray-800 p-8'>
+          
+        <h1 className='text-2xl font-bold text-center mb-5 '>
+          Password Generetor
+        </h1>
+
+       </div>
+    </div>
+  )
+}
+
+export default App
