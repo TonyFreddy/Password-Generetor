2860d244ff5ce6f115c111d19e5b0c0405043930
Tony <kounleadissa@gmail.com>
2026-03-26 03:03:54 +0100

    add a button function

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,56 @@
 import React from 'react'
 
-
 const App = () => {
   return (
     <div className='min-h-screen bg-gray-950 flex items-center justify-center px-4'>
-     <div className='bg-gray-900 text-gray-200 w-full max-w-md border rounded-2xl shadow-xl border-gray-800 p-8'>
-          
+      <div className='bg-gray-900 text-gray-200 w-full max-w-md border rounded-2xl shadow-xl border-gray-800 p-8'>
+
         <h1 className='text-2xl font-bold text-center mb-5'>
-          Password Generetor
+          Password Generator
         </h1>
-      
-      { /* Password Display */}
-      <div className='flex mb-4'>
 
-         <input type="text" placeholder='Generated Password' className='flex-1 bg-gray-800 border border-gray-700 
-         rounded-l-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500' readOnly />
-
+        {/* Password Display */}
+        <div className='flex mb-4'>
+          <input type="text" placeholder='Generated Password' className='flex-1 bg-gray-800 border border-gray-700 rounded-l-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500' readOnly />
           <button className='bg-indigo-600 hover:bg-indigo-700 px-4 rounded-r-lg transition'>
             Copy
-           </button>
+          </button>
+        </div>
+
+        {/* Password Length */}
+        <div className='mb-4'>
+          <label className='block mb-2 text-sm font-medium'>Password Length</label>
+          <input type="range" min="6" max="32" className="w-full accent-indigo-600" />
+        </div>
+
+        {/* Options */}
+        <div className='space-y-3 mb-6'>
+          <label className='flex items-center justify-between cursor-pointer'>
+            <span>Include UpperCase</span>
+            <input type="checkbox" className='accent-indigo-600 w-4 h-4' />
+          </label>
+          <label className='flex items-center justify-between cursor-pointer'>
+            <span>Include LowerCase</span>
+            <input type="checkbox" className='accent-indigo-600 w-4 h-4' />
+          </label>
+          <label className='flex items-center justify-between cursor-pointer'>
+            <span>Include Numbers</span>
+            <input type="checkbox" className='accent-indigo-600 w-4 h-4' />
+          </label>
+          <label className='flex items-center justify-between cursor-pointer'>
+            <span>Include Symbols</span>
+            <input type="checkbox" className='accent-indigo-600 w-4 h-4' />
+          </label>
+        </div>
+
+       
+        {/* Generate button */}
+        <button className='w-full bg-indigo-600 hover:bg-indigo-700 py-2 rounded-lg font-semibold transition'>
+          Generate Password
+        </button>
+
+
       </div>
-
-     { /* Password Length */}
-      <div className='mb-4'>
- 
-         <label className='block mb-2 text-5m font-medium'> Password Length </label>
-          <input 
-          type="range" 
-          min="6" 
-          max="32"
-          className="w-full accent-indigo-600"/>
-      </div>
-
-     {/* Option*/}
-     <div className='space-y-3 nb-6'>
-            <label className='flex items-center justify-between cursor-pointer'>
-               <span>Include UpperCase </span>
-               <input type="chechbox" className='accent-indigo-600' />
-            </label>
-
-                 <label className='flex items-center justify-between cursor-pointer'>
-               <span>Include LowerCase </span>
-               <input type="chechbox" className='accent-indigo-600' />
-            </label>
-
-                 <label className='flex items-center justify-between cursor-pointer'>
-               <span>Include Numbers </span>
-               <input type="chechbox" className='accent-indigo-600' />
-            </label>
-
-                 <label className='flex items-center justify-between cursor-pointer'>
-               <span>Include Symbols </span>
-               <input type="chechbox" className='accent-indigo-600' />
-            </label>
-     </div>
-
-     </div>
     </div>
   )
 }
